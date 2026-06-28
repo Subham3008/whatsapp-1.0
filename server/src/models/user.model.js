0ddd0352b5bf64fa4bf210f825f8861a092b59fa
Subham3008 <subhamsamanta065@gmail.com>
2026-06-28 23:32:56 +0530

    feat: create user model

---
+++ server/src/models/user.model.js
@@ -0,0 +1,42 @@
+import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    index: true,
+    trim: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    index: true,
+    lowercase: true,
+    trim: true
+  },
+  password: {
+    type: String,
+    required: true,
+    trim: true
+  }
+}, {
+  timestamps: true
+})
+
+userSchema.pre('save', async function () {
+  if (this.isModified('password')) {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+  }
+})
+
+userSchema.methods.comparePassword = async function (password) {
+  return await bcrypt.compare(password, this.password);
+}
+
+const userModel = mongoose.model("users", userSchema);
+
+export default userModel;
