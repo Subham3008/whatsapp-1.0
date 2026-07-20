87d7b755311cd08268bccac65e83e13d874efb9a
Subham3008 <subhamsamanta065@gmail.com>
2026-07-20 23:35:41 +0530

    model: create conversation model

---
+++ server/src/models/conversation.model.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose"
+
+const conversationSchema = new mongoose.Schema({
+  participants: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "users",
+    required: true
+  }],
+  lastMessageAt: {
+    type: Date,
+  }
+}, {
+  timestamps: true
+})
+
+const conversationModel = mongoose.model("Conversation", conversationSchema)
+
+export default conversationModel
